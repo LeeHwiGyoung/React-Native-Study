b782791fdb1ef69c9ee9aefd5a6af952e0368c53
HwiGyoung Lee <ter0000@naver.com>
2025-10-18 14:49:39 +0900

    ✨ (tabs) : TabNavigatior 에 사용될 View  기본 설정

---
+++ app/(tabs)/[username].tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Username() {
+  return (
+    <View>
+      <Text> this Is [username] </Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/add.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+
+export default function Add() {
+  return (
+    <View style={{flex : 1 , justifyContent : 'center' , alignItems : 'center'}}>
+      <Text>Eidt app/add.tsx to edit this screen</Text>
+    </View>
+  )
+}
