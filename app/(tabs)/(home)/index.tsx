6a616409f01d0854c0cb408ab7cc3f248885995a
HwiGyoung Lee <ter0000@naver.com>
2025-11-02 14:46:01 +0900

    ✨ index.tsx : safeAreaView 적용

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,30 +1,43 @@
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
   const pathname = usePathname();
+  const isLoggedIn = false;
   console.log("현재 경로:", pathname);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View>
-        <TouchableOpacity onPress={() => router.replace(`/`)}>
-          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
-            Home
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => router.replace(`/following`)}>
-          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
-            For you
-          </Text>
-        </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.headerLogo}
+          source={require("../../../assets/images/react-logo.png")}
+        />
+        {!isLoggedIn && (
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>로그인</Text>
+          </TouchableOpacity>
+        )}
       </View>
+      {isLoggedIn && (
+        <View style={styles.tabContainer}>
+          <View style={styles.tab}>
+            <TouchableOpacity onPress={() => router.replace(`/`)}>
+              <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+                Home
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.tab}>
+            <TouchableOpacity onPress={() => router.replace(`/following`)}>
+              <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+                For you
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
       <View>
         <TouchableOpacity onPress={() => router.push("/username/post/1")}>
           <Text>게시글1</Text>
@@ -40,6 +53,38 @@ export default function Index() {
           <Text>게시글3</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabContainer: {
+    flexDirection: "row",
+  },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+  },
+  headerLogo: {
+    width: 42,
+    height: 42,
+  },
+  loginButton: {
+    position: "absolute",
+    right: 20,
+    top: 0,
+    backgroundColor: "#000",
+    borderRadius: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+  },
+  loginButtonText: {
+    color: "#fff",
+  },
+});
