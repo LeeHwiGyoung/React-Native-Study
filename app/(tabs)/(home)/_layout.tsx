d7f067c12384c4c8c65a09041e3bac24cde79092
HwiGyoung Lee <ter0000@naver.com>
2025-11-11 17:47:14 +0900

    🎨 SafeAreaProvider 적용 : 컴포넌트를 공유하며 화면을 전환할 때 깜빡임 문제 해결

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,5 +1,12 @@
 import { Slot } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeLayout() {
-  return <Slot />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Slot />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
