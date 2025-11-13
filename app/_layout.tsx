0904e7fb839dcde818b2447592340fe4a247e208
HwiGyoung Lee <ter0000@naver.com>
2025-11-13 15:10:00 +0900

    ✨ 앱 실행시 asyncStore 를 확인하여 로그인 유지

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,29 @@
 import { router, Stack } from "expo-router";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Alert } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+interface User {
+  id: string;
+  name: string;
+  profileImageUrl: string;
+  description: string;
+}
+
 export const AuthContext = createContext<{
-  user?: object;
+  user?: User | null;
   login?: () => Promise<void>;
-  logout?: () => Promise<void>;
-}>({});
+  logout?: () => Promise<any>;
+}>({
+  user: null,
+});
+
 export default function RootLayout() {
-  const [user, setUser] = useState(null);
-  const login = () => {
-    fetch("/login", {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = async () => {
+    return await fetch("/login", {
       method: "POST",
       body: JSON.stringify({
         username: "hwigyoung",
@@ -45,6 +56,14 @@ export default function RootLayout() {
       AsyncStorage.removeItem("user"),
     ]);
   };
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then((user) => {
+      setUser(user ? JSON.parse(user) : null);
+    });
+    // 엑세스 토큰 유효성 체크를 해야함
+  }, []);
+
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       <Stack screenOptions={{ headerShown: false }}>
