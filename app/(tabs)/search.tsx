3952d4c64dc0c78b27ba6621d82e62b8bfdfd2e7
HwiGyoung Lee <ter0000@naver.com>
2025-11-22 18:11:08 +0900

    ✨search 화면에서 팔로우 제안 기능 추가

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,23 +1,38 @@
 import SideMenu from "@/components/SideMenu";
 import { Ionicons } from "@expo/vector-icons";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import {
+  FlatList,
+  Image,
   Pressable,
   StyleSheet,
+  Text,
   TextInput,
   useColorScheme,
   View,
 } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { AuthContext } from "../_layout";
+import FollowSuggest, { FollowSuggestUser } from "@/components/FollowSuggest";
 
 export default function Saech() {
   const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [followSuggestUsers, setFollowSuggestUsers] =
+    useState<FollowSuggestUser[]>();
   const { user } = useContext(AuthContext);
   const isLoggedIn = !!user;
   const insets = useSafeAreaInsets();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setFollowSuggestUsers(data.users);
+        console.log("search", data.users);
+      });
+  }, []);
   return (
     <View
       style={[
@@ -43,6 +58,10 @@ export default function Saech() {
             />
           </Pressable>
         )}
+        <Image
+          source={require("../../assets/images/react-logo.png")}
+          style={styles.logo}
+        />
       </View>
       <View style={styles.searchBar}>
         <TextInput
@@ -55,6 +74,17 @@ export default function Saech() {
           placeholder="Search"
           value={searchQuery}
           onChangeText={setSearchQuery}
+        />
+      </View>
+      <View style={styles.followSuggestContainer}>
+        <Text style={styles.followSuggestText}>Follow suggestions</Text>
+        <FlatList
+          data={followSuggestUsers}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <FollowSuggest item={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: insets.bottom }}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
         />
       </View>
     </View>
@@ -81,6 +111,10 @@ const styles = StyleSheet.create({
   headerLight: {
     backgroundColor: "white",
   },
+  logo: {
+    width: 40,
+    height: 40,
+  },
   sideMenu: {
     position: "absolute",
     top: 0,
@@ -88,8 +122,6 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
   },
   searchBar: {
-    flex: 1,
-    justifyContent: "center",
     alignItems: "center",
   },
   searchBarInput: {
@@ -104,5 +136,21 @@ const styles = StyleSheet.create({
   },
   searchInputTextDark: {
     color: "white",
+  },
+  followSuggestContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  followSuggestText: {
+    fontSize: 18,
+    fontWeight: "700",
+    paddingVertical: 12,
+  },
+  separator: {
+    height: 1, // 구분선의 높이
+    marginLeft: 50,
+    marginVertical: 4, // 위아래 간격
+    width: "100%",
+    backgroundColor: "#eeeeee",
   },
 });
