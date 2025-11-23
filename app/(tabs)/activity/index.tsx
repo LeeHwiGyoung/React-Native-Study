2d52ab551248a3755eee555d13c747d364f50faa
HwiGyoung Lee <ter0000@naver.com>
2025-11-23 17:01:20 +0900

    ✨activity/index.tsx : 포스트 목록을 간단하게 보여주는 기능 추가

--- app/(tabs)/activity/index.tsx
+++ app/(tabs)/activity/index.tsx
@@ -1,174 +1,30 @@
-import NotFound from "@/app/+not-found";
-import { AuthContext } from "@/app/_layout";
-import SideMenu from "@/components/SideMenu";
-import { Ionicons } from "@expo/vector-icons";
-import { usePathname, useRouter } from "expo-router";
-import { useContext, useState } from "react";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  useColorScheme,
-  View,
-} from "react-native";
+import FollowSimpleActivityItem from "@/components/FollowSimpleActivityItem";
+import { TPost } from "@/components/Post";
+import { useEffect, useState } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function Activity() {
-  const router = useRouter();
-  const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
-  const { user } = useContext(AuthContext);
-  const isLoggedIn = !!user;
-  const pathname = usePathname(); // 현재 주소를 반환하는 훅
+export default function Index() {
+  const [followPost, setFollowPost] = useState<TPost[]>([]);
   const insets = useSafeAreaInsets();
-  const colorScheme = useColorScheme();
-  if (
-    ![
-      "/activity",
-      "/activity/follows",
-      "/activity/replies",
-      "/activity/reposts",
-      "/activity/mentions",
-      "/activity/quotes",
-      "/activity/verified",
-    ].includes(pathname)
-  ) {
-    return <NotFound />;
-  }
+
+  useEffect(() => {
+    fetch("/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setFollowPost(data.posts);
+      });
+  }, []);
+
   return (
-    <View
-      style={[
-        styles.container,
-        { paddingTop: insets.top },
-        colorScheme === "dark" ? styles.containerDark : styles.containerLight,
-      ]}
-    >
-      <SideMenu
-        isVisible={isSideMenuVisible}
-        onClose={() => setIsSideMenuVisible(false)}
+    <View style={styles.container}>
+      <FlatList
+        contentContainerStyle={{ paddingBottom: insets.bottom }}
+        data={followPost}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <FollowSimpleActivityItem item={item} />}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
-      <View
-        style={[
-          styles.header,
-          colorScheme === "dark" ? styles.headerDark : styles.headerLight,
-        ]}
-      >
-        {isLoggedIn && (
-          <Pressable
-            onPress={() => setIsSideMenuVisible(true)}
-            style={styles.sideMenu}
-          >
-            <Ionicons
-              name="menu"
-              size={24}
-              color={colorScheme === "dark" ? "white" : "black"}
-            />
-          </Pressable>
-        )}
-      </View>
-      <View>
-        <View style={styles.tabBar}>
-          <TouchableOpacity onPress={() => router.push(`/activity`)}>
-            {/* button과 유사 */}
-            <Text
-              style={{
-                color:
-                  pathname === "/activity"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              All
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push(`/activity/follows`)}>
-            <Text
-              style={{
-                color:
-                  pathname === "/activity/follows"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              Follows
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push(`/activity/replies`)}>
-            <Text
-              style={{
-                color:
-                  pathname === "/activity/replies"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              Replies
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push(`/activity/mentions`)}>
-            <Text
-              style={{
-                color:
-                  pathname === "/activity/mentions"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              Mentions
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push(`/activity/quotes`)}>
-            <Text
-              style={{
-                color:
-                  pathname === "/activity/quotes"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              Quotes
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push(`/activity/reposts`)}>
-            <Text
-              style={{
-                color:
-                  pathname === "/activity/reposts"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              Reposts
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push(`/activity/verified`)}>
-            <Text
-              style={{
-                color:
-                  pathname === "/activity/verified"
-                    ? "red"
-                    : colorScheme === "dark"
-                      ? "white"
-                      : "black",
-              }}
-            >
-              Verified
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
     </View>
   );
 }
@@ -177,31 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  containerDark: {
-    backgroundColor: "#101010",
-  },
-  containerLight: {
-    backgroundColor: "white",
-  },
-  header: {
-    alignItems: "center",
-    height: 50,
-    justifyContent: "space-between",
-  },
-  headerDark: {
-    backgroundColor: "#101010",
-  },
-  headerLight: {
-    backgroundColor: "white",
-  },
-  sideMenu: {
-    position: "absolute",
-    top: 0,
-    left: 20,
-    paddingVertical: 8,
-  },
-  tabBar: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+  separator: {
+    height: 1, // 구분선의 높이
+    marginLeft: 50,
+    marginVertical: 4, // 위아래 간격
+    width: "100%",
+    backgroundColor: "#eeeeee",
   },
 });
